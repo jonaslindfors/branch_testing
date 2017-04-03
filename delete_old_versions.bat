39f4aebae7e037e86beb8838c5b6dc5d11ff4af2
jonaslindfors <jonas.lindfors@chimneygroup.com>
2017-04-03 13:22:52 +0200

    committing two files only to sto branch

---
+++ delete_old_versions.bat
@@ -0,0 +1,7 @@
+@echo off
+echo ==============================================
+echo    Chimney Delete Old Versions 2.0
+echo ==============================================
+echo.
+C:/Python27/python.exe C:/Users/Jonas/Documents/GitHub/delete_old_versions/delete_old_versions.py %1
+pause
